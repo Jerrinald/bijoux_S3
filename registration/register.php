d461de6055317925ff251d7edde1f37d66d1b6da
Jerrinald <74157700+Jerrinald@users.noreply.github.com>
2020-11-08 23:11:31 +0100

    Add files via upload

---
+++ registration/register.php
@@ -0,0 +1,111 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style1.css" />
+<link rel="stylesheet" href="../styles.css" />
+</head>
+<body>
+
+<header id="header" class="header">
+    <div class="navigation">
+      <div class="container">
+        <nav class="nav">
+          <div class="nav__hamburger">
+            <svg>
+              <use xlink:href="../images/sprite.svg#icon-menu"></use>
+            </svg>
+          </div>
+
+          <div class="nav__logo">
+            <a href="/" class="scroll-link">
+              DIAMANT
+            </a>
+          </div>
+
+          <div class="nav__menu">
+            <div class="menu__top">
+              <span class="nav__category">DIAMANT</span>
+              <a href="#" class="close__toggle">
+                <svg>
+                  <use xlink:href="../images/sprite.svg#icon-cross"></use>
+                </svg>
+              </a>
+            </div>
+            <ul class="nav__list">
+              <li class="nav__item">
+                <a href="#header" class="nav__link scroll-link">Acceuil</a>
+              </li>
+              <li class="nav__item">
+                <a href="#category" class="nav__link scroll-link">Categorie</a>
+              </li>
+              <li class="nav__item">
+                <a href="#news" class="nav__link scroll-link">Blog</a>
+              </li>
+              <li class="nav__item">
+                <a href="#contact" class="nav__link scroll-link">Contact</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="nav__icons">
+            <a href="login.php" class="icon__item">
+              <svg class="icon__user">
+                <use xlink:href="../images/sprite.svg#icon-user"></use>
+              </svg>
+            </a>
+
+            <a href="#" class="icon__item">
+              <svg class="icon__search">
+                <use xlink:href="../images/sprite.svg#icon-search"></use>
+              </svg>
+            </a>
+
+            <a href="#" class="icon__item">
+              <svg class="icon__cart">
+                <use xlink:href="../images/sprite.svg#icon-shopping-basket"></use>
+              </svg>
+              <span id="cart__total">0</span>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </div>
+   </header>
+
+<?php
+require('config.php');
+if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['password'])){
+	// récupérer le nom d'utilisateur et supprimer les antislashes ajoutés par le formulaire
+	$username = stripslashes($_REQUEST['username']);
+	$username = mysqli_real_escape_string($conn, $username); 
+	// récupérer l'email et supprimer les antislashes ajoutés par le formulaire
+	$email = stripslashes($_REQUEST['email']);
+	$email = mysqli_real_escape_string($conn, $email);
+	// récupérer le mot de passe et supprimer les antislashes ajoutés par le formulaire
+	$password = stripslashes($_REQUEST['password']);
+	$password = mysqli_real_escape_string($conn, $password);
+	//requéte SQL + mot de passe crypté
+    $query = "INSERT into `users` (username, email, password)
+              VALUES ('$username', '$email', '".hash('sha256', $password)."')";
+	// Exécute la requête sur la base de données
+    $res = mysqli_query($conn, $query);
+    if($res){
+       echo "<div class='sucess'>
+             <h3>Vous êtes inscrit avec succès.</h3>
+             <p>Cliquez ici pour vous <a href='login.php'>connecter</a></p>
+			 </div>";
+    }
+}else{
+?>
+<form class="box" action="" method="post">
+	<h1 class="box-logo box-title"><a href="../index.html">Bijoux.com</a></h1>
+    <h1 class="box-title">S'inscrire</h1>
+	<input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur" required />
+    <input type="text" class="box-input" name="email" placeholder="Email" required />
+    <input type="password" class="box-input" name="password" placeholder="Mot de passe" required />
+    <input type="submit" name="submit" value="S'inscrire" class="box-button" />
+    <p class="box-register">Déjà inscrit? <a href="login.php">Connectez-vous ici</a></p>
+</form>
+<?php } ?>
+</body>
+</html>
